9724b7f0071e7b578c824d85fb0f613dabbc2484
gaponukz <gaponukz54@gmail.com>
2023-08-26 21:00:27 +0300

    Fix error

--- src/application/dto.ts
+++ src/application/dto.ts
@@ -3,4 +3,10 @@ export class CreateUserDTO {
     username: string
     startPreiodDate: Date
     endPreiodDate: Date
+
+    constructor (username: string, startPreiodDate: Date, endPreiodDate: Date) {
+        this.username = username
+        this.startPreiodDate = startPreiodDate
+        this.endPreiodDate = endPreiodDate
+    }
 }
