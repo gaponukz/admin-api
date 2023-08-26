d558deeb0251af17cb8bed858bef4bab975e324e
gaponukz <gaponukz54@gmail.com>
2023-08-26 21:16:01 +0300

    Try to fix CI

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,13 @@
 /* eslint-env node */
 module.exports = {
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      './node_modules/gts/.eslintrc.json',
+    ],
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint'],
+    rules: {},
     root: true,
-};
+  };
+  
